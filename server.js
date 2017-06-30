67fffdf51ef423431f30490e3a64e2966864dd38
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2017-06-30 17:24:18 -0400

    ports

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-var app = require('./distServer');
+var npmRun = require('npm-run');
+
+npmRun.exec('npm run build-server', { cwd: __dirname }, function (err, stdout, stderr) {
+    var app = require('./distServer');
+})
