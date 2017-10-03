1dc8d0b973d86ae0b9a04e55bc1bbaad61700c3d
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2017-10-03 15:57:04 +0300

    RI=KI for now

--- src/content/config.js
+++ src/content/config.js
@@ -2,8 +2,8 @@ export default {
 	"refugee.info": {
 		theme: "irc",
 		accessToken:
-			"0cd5c6bfefa4f7f33a98c487012e6f34c2031aee1ff2f6858b11be592ab349d9",
-		space: "ktglofo8nf88",
+			"55bea7f7ed5d0e812aa1d48e378dd9a2edb8f9f78c17bd9d2ffa6a014ea3ee2b",
+		space: "l4oa58axtceh",
 		languages: [["en", "English"], ["ar", "العربيـة"]],
 		logo: "/logo.png",
 		title: "Refugee Info",
