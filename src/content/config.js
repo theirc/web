5ce9fa755a0bff558fc2eddc2ac6044505d016c5
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2017-10-03 12:41:58 +0300

    Fix to the KI config

--- src/content/config.js
+++ src/content/config.js
@@ -1,13 +1,14 @@
 export default {
 	"refugee.info": {
 		theme: "irc",
-		accessToken: "",
-		space: "",
+		accessToken:
+			"55bea7f7ed5d0e812aa1d48e378dd9a2edb8f9f78c17bd9d2ffa6a014ea3ee2b",
+		space: "l4oa58axtceh",
 		languages: [["en", "English"], ["ar", "العربيـة"]],
 		logo: "/logo.png",
 		title: "Refugee Info",
 	},
-	localhost: {
+	"khabrona.info": {
 		theme: "mc",
 		accessToken:
 			"55bea7f7ed5d0e812aa1d48e378dd9a2edb8f9f78c17bd9d2ffa6a014ea3ee2b",
