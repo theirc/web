f7be3b677f18ff1228025af22fef3f0b172e4c69
Andres <andresd.aguilar@gmail.com>
2018-01-26 10:53:03 -0300

    category article list

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { translate } from "react-i18next";
+
+class ArticleList extends React.Component {
+    render() {
+        <div>
+            <h1>Article List</h1>
+        </div>
+    }
+}
+
+export default translate()(ArticleList);
--- src/scenes/CategoryHome.js
+++ src/scenes/CategoryHome.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import services from '../backend';
 import { connect } from 'react-redux'
-import {
-} from '../components';
+import { ArticleList } from '../components'
+import { push } from "react-router-redux";
+
+const Remarkable = require("remarkable");
+
+const md = new Remarkable("full", {
+	html: true,
+	linkify: true,
+	typographer: true,
+	breaks: true,
+});
 
 class CategoryHome extends React.Component {
     constructor() {
@@ -15,14 +24,44 @@ class CategoryHome extends React.Component {
     }
 
     render() {
-        const { country, category } =this.props;
+        const { country, category, onNavigate } =this.props;
 
         if(!country || !category) {
             return null;
         }
 
-        return (<div>
-        </div>);
+        return (
+            <div className="SearchPage">
+                <div className="Title">
+                    <h1>
+                        {category.fields.name}"
+                    </h1>
+                </div>
+                <div className="results">
+                    {category.fields.articles && category.fields.articles.map((article, i) => {
+                        let hero = article.fields.hero;
+
+                        return [
+                            i > 0 && <hr className="line" key={`hr-${article.sys.id}`} />,
+                            <div
+                                key={article.sys.id}
+                                className="Article"
+                                onClick={() => onNavigate(`/${article.fields.country.fields.slug}/${article.fields.category.fields.slug}/${article.fields.slug}`)}
+                            >
+                                {article.fields.hero && <div className="Image" style={{ backgroundImage: `url('${article.fields.hero.fields.file.url}')` }} />}
+                                <div className={`Text ${article.fields.hero ? 'TextWithImage' : ''}`}>
+                                    <h2> {article.fields.title}</h2>
+                                    <p dangerouslySetInnerHTML={{ __html: md.render(article.fields.lead)}} />
+                                </div>
+                                <s className="Read-More">
+                                    <a href="#">Read More</a>
+                                </s>
+                            </div>,
+                        ];
+                    })}
+                </div>
+            </div>
+            );
     }
 }
 
@@ -34,6 +73,9 @@ const mapState = (s, p) => {
 };
 const mapDispatch = (d, p) => {
     return {
+        onNavigate(url) {
+			d(push(url));
+		},
         onMount: () => {
             d(services.articles.get('the-title-of-this-article-style-title'));
         }
