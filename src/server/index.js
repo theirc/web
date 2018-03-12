d619b1b65836dddd776c7489a24777e8361a9185
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2018-03-12 12:05:43 -0400

    Fixed article list +  actual fix to server

--- src/server/index.js
+++ src/server/index.js
@@ -293,9 +293,9 @@ app.get("/:country/:category/:article", function (req, res, err) {
 									.filter(i => i.fields.country.fields.slug === country && i.fields.category.fields.slug === category));
 								if (!match) {
 									let _cnt = _.first(cc.items);
-									let _cat = _.first(_cnt.fields.categories.filter(x => x.fields.slug === category));
+									let _cat = _.first(_cnt.fields.categories.filter(x => x.fields && x.fields.slug === category));
 									if (_cat) {
-										match = _.first(_cat.fields.articles.filter(x => x.fields.slug === article));
+										match = _.first(_cat.fields.articles.filter(x => x.fields &&  x.fields.slug === article));
 									}
 								}
 
