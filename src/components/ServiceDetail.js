7bd00d2c3e029e81da48dd799524c738470feb6d
nciampechini <nahuel.ciampechini@thinkersys.com>
2019-04-02 14:48:02 -0300

    SP-637: change on the contact information buttons style

--- src/components/ServiceDetail.js
+++ src/components/ServiceDetail.js
@@ -141,7 +141,7 @@ class ServiceDetail extends React.Component {
 				<div className="Selector" onClick={() => window.open(action)}>
 					<h1>
 						<div style={{
-							display: 'inline-block', direction: 'ltr',
+							display: 'inline-block',
 							overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', width:'650px'
 						}}>
 							{typeText}{text}
