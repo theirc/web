2cc104052998d1011c5fb7f3866888fcf6c4c5ca
Andres Aguilar <andres.aguilar@rescue.org>
2019-07-05 13:59:32 -0300

    convert font to ttf

--- public/service-worker2.js
+++ public/service-worker2.js
@@ -19,7 +19,7 @@ var urlsToCache = [
       '/sockjs-node/info?t=1557509894850',
       '/css/material-icons.css',
       '/css/ionicons.min.css',  
-      '/fonts/MaterialIcons-Regular.woff2',
+      '/fonts/MaterialIcons-Regular.ttf',
       '/fonts/fontawesome-webfont.woff2',
       '/css/fac4bfa814.css',
       '/css/fontawesome.min.css',
