6ab585631c673cec1da0727c4e751c3760fc6bf5
Juan Bressan <juan.bressan@rescue.org>
2019-08-01 22:48:10 -0300

    SP-1303: multilanguage added to select

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -91,7 +91,7 @@ class CategoryList extends Component {
 				{/* <HeaderBar title={t("Categories").toUpperCase()} /> */}
 				<div className='tiles-desktop'>
 					<select className='select-css' value={this.state.selectedCategory} onChange={this.onChange}>
-						<option value=''>All Categories</option>
+						<option value=''>{t('All Categories')}</option>
 						{(categories || []).filter(showCategory).map(e => <option key={e.sys.id} value={e.sys.id}>{e.fields.name}</option>)}
 					</select>
 					<ul>
