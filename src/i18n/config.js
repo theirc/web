db741b77456a8d7c743ef32bb1987cd4673811c3
Juan Bressan <juan.bressan@rescue.org>
2019-09-10 13:16:53 -0300

    results for string added

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -152,6 +152,7 @@ export default {
             "Instance moved Bulgaria banner": "متاسفیم! راهنمای پناهندگان دیگر اطلاعاتی در مورد بلغارستان ارائه نمی‌کند.",
             "Instance moved Bulgaria small": "به جای آن، به بنیاد دسترسی به حقوق کمک کردیم تا وبسایت جدیدی برای پناهندگان در بلغارستان ایجاد کنند: refugeelife.bg",
             "Instance moved Bulgaria label": "از سایت www.refugeelife.bg بازدید بفرمایید.",
+            "Results for": "Results for",
         },
     },
     ar: {
@@ -307,6 +308,7 @@ export default {
             "Instance moved Bulgaria banner": "عذراً! توقف دليل اللاجئ عن تزويد معلومات عن بلغاريا.",
             "Instance moved Bulgaria small": "عوضاً عن ذلك، قمنا بمساعدة منظمة الوصول إلى الحقوق (Foundation for Access to Rights) في إنشاء موقع للاجئين في بلغاريا: refugeelife.bg",
             "Instance moved Bulgaria label": "قم بزيارة www.refugeelife.bg",
+            "Results for": "Results for",
         },
     },
     ur: {
@@ -462,6 +464,7 @@ export default {
             "Instance moved Bulgaria banner": "ہم معذرتخواہ ہیں ریگیوجی۔انفو اب بلغاریہ کے بارے میں معلومات فراہم نہیں کرتا.",
             "Instance moved Bulgaria small": "اس کے بجائے، ہم نے بلغاریہ میں پناہ گزینوں کے لئے  فاونڈیشن فار ایکسس ٹو رائٹس کی نئی ویب سائٹ بنانے میں مدد کی ہے: refugeelife.bg",
             "Instance moved Bulgaria label": "پر جائیں www.refugeelife.bg",
+            "Results for": "Results for",
         },
     },
     es: {
@@ -602,6 +605,7 @@ export default {
             "No services found with the keywords used": "No se encontraron servicios con las palabras clave utilizadas.",
 			"COOKIES_BANNER": "Utilizamos cookies para ofrecer nuestros servicios. Al continuar utilizando este sitio, acepta dicho uso y reconoce que ha leído y entendido nuestra política de cookies ({{cookiePolicy}}) y política de privacidad ({{privacyPolicy}}).",
 			"Services in": "Servicios en",
+            "Results for": "Resultados para",
         }
     },
     fr: {
@@ -754,6 +758,7 @@ export default {
             "Rome":"Rome",
             "Turin":"Turin",
             "Ventimiglia":"Vintimille",
+            "Results for": "Results for",
         }
     },
     ti: {
@@ -888,6 +893,7 @@ export default {
             "No services found with the keywords used": "No services found with the keywords used",
             "COOKIES_BANNER": "ኣገልግሎትና ንምሃብ ኩኪስ(Cookies) ንጥቀም ኢና። ነዚ መርበብ’ዚ ንኽትጥቀመሉ ብምቕጻልካ ድማ፣ ነዚ ኣጠቓቕማ’ዚ ተቐቢልካዮን ንናይ ኩኪ ፖሊሲን ({{cookiePolicy}})  ፖሊሲ ምስጢራውነትን ኣንቢብካ ከም ዝተረዳእካዮ ኣፍልጦ ትህብ ኣለኻ ማለት እዩ። ({{privacyPolicy}})",
             "Services in": "ኣገልግሎታት ኣብ",
+            "Results for": "Results for",
         }
     },
     en: {
@@ -1047,6 +1053,7 @@ export default {
             "Instance moved Bulgaria banner": "Sorry! Refugee.Info has stopped providing information about Bulgaria. Refugee.info no longer hosts content on Bulgaria.",
             "Instance moved Bulgaria small": "Instead, we helped the Foundation for Access to Rights create a new site for refugees in Bulgaria: refugeelife.bg.",
             "Instance moved Bulgaria label": "Visit www.refugeelife.bg",
+            "Results for": "Results for",
         },
     }
 };
