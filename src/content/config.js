85efcaa6488e8e9297807dc9f012ab2be19391f7
Andrés Aguilar <andres.aguilar@rescue.org>
2019-10-29 13:39:32 -0300

    update preview key for staging

--- src/content/config.js
+++ src/content/config.js
@@ -204,8 +204,7 @@ module.exports = {
 	"staging.cuentanos.org": {
 		theme: "generic",
 		siteCode: "CN",
-		accessToken: "2a64ff636dd4e5b44bd21afcb7f8d77c5c238e3ebe387ec6047a0b9d5c6a704d",
-		host: "preview.contentful.com",
+		accessToken: "98cd6321d5578bab8cb4c9bafe022f2bd73db5ffce296420d934008f9800fb80",
 		space: "e17qk44d7f2w",
 		languages: [["es", "Español"]],
 		logo: "/images/cn.tb.png",
