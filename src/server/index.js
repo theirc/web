7ec4ba620332f126442382d79e99e09a8c166f03
Andrés Aguilar <andres.aguilar@rescue.org>
2019-11-21 23:28:50 -0300

    update server to allow subscription

--- src/server/index.js
+++ src/server/index.js
@@ -196,56 +196,6 @@ app.get("/preview/:serviceId/", function(req, res, err) {
     }
 });
 
-// app.get("/direct/:article/", function(req, res, err) {
-//     const selectedLanguage = parseLanguage(req);
-//     let configKey = _.first(
-//         Object.keys(conf).filter(k => {
-//             return req.headers.host.indexOf(k) > -1;
-//         })
-//     );
-//     let context = {};
-//     const {
-//         article
-//     } = req.params;
-    
-//     try {
-//         if (configKey) {
-// 			const {
-// 				accessToken,
-// 				space
-// 			} = conf[configKey];
-// 			languageDictionary = Object.assign(languageDictionary, conf[configKey]);				
-// 			let cms = cmsApi(conf[configKey], languageDictionary);
-// 			cms.client
-// 				.getEntries({
-// 					content_type: "article",
-// 					"fields.slug": article,
-// 					locale: languageDictionary[selectedLanguage] || selectedLanguage,
-// 				})
-// 				.then(c => {	
-// 					if (c.items.length || [] >0){
-//                         let match = c.items[0];
-// 						if (match.fields.category && match.fields.country){
-// 							res.redirect(`/${match.fields.country.fields.slug}/${match.fields.category.fields.slug}/${article}/?language=${selectedLanguage}`);
-// 						}else{
-// 							res.redirect("/");
-// 						}
-// 					}
-// 				})
-// 				.catch(e => {
-// 					console.log("error", e);
-// 					//res.redirect(`/${country}/`);
-// 				});
-           
-//         }
-//     } catch (e) {
-//         mainRequest({
-//             description: e.toString(),
-//             image: ""
-//         })(req, res, err);
-//     }
-	
-// });
 
 app.get("/:country/services/:serviceId/", function(req, res, err) {
     const selectedLanguage = parseLanguage(req);
@@ -304,7 +254,13 @@ app.get("/:country/services/", function(req, res, err) {
         }
     });
 });
-
+app.get('/:country/:subscribe/:category', function(req, res, err){
+    return mainRequest({
+        title: "Subscripción",
+        description: "Subscrición a notificaciones",
+        image: "",
+    })(req, res, err);
+})
 app.get("/:country/:category/:article", function(req, res, err) {
     const selectedLanguage = parseLanguage(req);
     let configKey = _.first(
