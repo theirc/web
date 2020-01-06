e07708724a95e91de15483acf37c54bce884f6c4
Andres Aguilar <andresaguilar@MacBook-Pro-de-Andres.local>
2020-01-06 00:02:14 -0300

    get articles

--- src/server/twilio-routes.js
+++ src/server/twilio-routes.js
@@ -8,24 +8,21 @@ let {
 	languageDictionary
 } = conf;
 
+
 module.exports = function(app) {
     app.get("/twilio", (req,res) =>{
         res.status(200).json({'test': 'working'})
     })
-    app.get("/get-phone-tree/:slug", (req, res ,err) => {
-        console.log("init");
+    app.get("/get-phone-tree-home/:slug", (req, res ,err) => {
         const selectedLanguage = parseLanguage(req);
-        console.log(selectedLanguage);
         let configKey = _.first(
             Object.keys(conf).filter(k => {
                 return req.headers.host.indexOf(k) > -1;
             })
         );
-        console.log("config", configKey);
         const {
             slug,
         } = req.params;
-        console.log("slug",slug);
         
         languageDictionary = Object.assign(languageDictionary, conf[configKey]);
         let cms = cmsApi(conf[configKey], languageDictionary);
@@ -38,17 +35,98 @@ module.exports = function(app) {
             })
             .then(c => {
                 res.contentType("application/json");
-                res.send(c.items[0].fields.initial_message);
+                res.send(c.items[0].fields.initial_message.trim());
             })
             .catch(e => {
-                console.log(e);
                 res.redirect(`/${country}/`);
             });
         }catch (e) {
             console.log("ERROR", e);
         }
-        
-        
     });
+    app.get("/get-phone-tree-moderator/:slug", (req, res ,err) => {
+        const selectedLanguage = parseLanguage(req);
+        let configKey = _.first(
+            Object.keys(conf).filter(k => {
+                return req.headers.host.indexOf(k) > -1;
+            })
+        );
+        const {
+            slug,
+        } = req.params;
+        
+        languageDictionary = Object.assign(languageDictionary, conf[configKey]);
+        let cms = cmsApi(conf[configKey], languageDictionary);
+        try{
+            cms.client
+            .getEntries({
+                content_type: "phoneTreeMessage",
+                "fields.slug": slug,
+                locale: languageDictionary[selectedLanguage] || selectedLanguage,
+            })
+            .then(c => {
+                res.contentType("application/json");
+                res.send(c.items[0].fields.how_can_i_help.trim());
+            })
+            .catch(e => {
+                res.redirect(`/${country}/`);
+            });
+        }catch (e) {
+            console.log("ERROR", e);
+        }
+    });
+    app.get("/get-article/:slug/:category/:article", (req, res, err) => {
+        const selectedLanguage = parseLanguage(req);
+        let configKey = _.first(
+            Object.keys(conf).filter(k => {
+                return req.headers.host.indexOf(k) > -1;
+            })
+        );
+        const {
+            slug,
+            category,
+            article
+        } = req.params;
+        
+        languageDictionary = Object.assign(languageDictionary, conf[configKey]);
+        let cms = cmsApi(conf[configKey], languageDictionary);
+        let locale = languageDictionary[selectedLanguage] || selectedLanguage;
+        try{
+            cms.client
+            .getEntries({
+                content_type: "phoneTreeMessage",
+                "fields.slug": slug,
+                locale: locale,
+            })
+            .then(c => {
+                console.log(c.items[0].fields.options[category-1].fields.article_Detail[article-1]);
+                let id = c.items[0].fields.options[category-1].fields.article_Detail[article-1].sys.id;
+                cms.client
+                .getEntry(id, {
+                    locale: locale,
+                })
+                .then( a => {
+                    console.log(a.fields);
+                    //let category = a.fields.category.sys.id;
+                    // let entities = cms.client.parseEntries(a);
+                    // console.log("entities", entities);
+                    res.contentType("application/json");
+                    let article = { title: a.fields.title, sumary: a.fields.content.substr(0,200)+"...", link: "http://www.cuentanos.org"};
+                    res.send(article);
+                })
+                .catch(e => {
+                    console.log(e);
+                    res.send(e);
+                })
 
+                // res.contentType("application/json");
+                // res.send(c.items[0].fields.initial_message.trim());
+            })
+            .catch(e => {
+                res.redirect(`/${country}/`);
+            });
+        }catch (e) {
+            console.log("ERROR", e);
+        }
+    })
 }
