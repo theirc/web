55680cc74f17e85ddcc3f0ee3d179e8ca8e8a003
Juan Bressan <juan.bressan@rescue.org>
2020-01-07 14:20:05 -0300

    config files for Khabrona updated

--- src/config/khabrona.info/countries/jordan.js
+++ src/config/khabrona.info/countries/jordan.js
@@ -1,11 +1,15 @@
 export default {
-	phoneCode: '', 
+	countryCode: '', 
 	customQuestionLink: [],
 	flag: '',
-	showArticles: true,
-	showServices: false,
 	languages: [["en", "English"], ["ar", "العربيـة"]],
 	questionLink: 'mailto:jo-khabrona-info@mercycorps.org',
+	switches: {
+		showArticles: true,
+		showDepartments: false,
+		showLinkToAdministration: false,
+		showServices: false,
+	},
 	thirdParty: {
 		facebook: {
 			appId: '154471931830995',
--- src/config/khabrona.info/instance.js
+++ src/config/khabrona.info/instance.js
@@ -1,25 +1,25 @@
 
 export default {
 	brand: {
+		code: 'ki',
 		images: {
 			favicon: "/images/favicons/ki-favicon.png",
 			logo: "/images/logos/logo-ki-white.png",
 			logoBlack: "/images/logos/logo-ki-black.png",
 			thumbnail: "/images/logos/ki-thumbnail.png",
 			},
-		code: 'ki',
 		name: 'Khabrona Info',
 		tabTitle: 'Khabrona.Info',
 		theme: 'mc',
 		url: 'khabrona.info'
 	},
 	countries: {
+		'jordan': require('./countries/jordan').default,
 	},
 	env: null,
 	switches: {
 		cookieBanner: false,
 		disableCountrySelector: true,
 		disableLanguageSelector: false,
-		showDepartments: true,
 	}
 };
