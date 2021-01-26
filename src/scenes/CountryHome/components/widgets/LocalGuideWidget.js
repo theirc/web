28cf4cd11d1edbc5c44cde1d8c301aa6dddde88e
Guiso92 <emilioguisolfo@gmail.com>
2021-01-26 13:11:25 -0800

    tmp changes

--- src/scenes/CountryHome/components/widgets/LocalGuideWidget.js
+++ src/scenes/CountryHome/components/widgets/LocalGuideWidget.js
@@ -1,46 +1,53 @@
 // libs
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { translate } from "react-i18next";
-import _ from 'lodash';
+import _ from "lodash";
 
 // local
 import "./LocalGuideWidget.css";
 
-const NS = { ns: 'CountryHome' };
+const NS = { ns: "CountryHome" };
 
 class LocalGuideWidget extends Component {
+  render() {
+    const { country, language, t, guideItems } = this.props;
 
-	
-	render() {
-		const { country, language, t, guideItems } = this.props;
+    /*jshint ignore:start*/
+    /*eslint-disable*/
+    return (
+      <div className="LocalGuide LocalGuideWidget">
+        <div className="top-container">
+          <div className="title-line"></div>
+          <h3>{t("guide.Local Guide", NS)}</h3>
+        </div>
 
-		/*jshint ignore:start*/
-		/*eslint-disable*/
-		return (
-			<div className="LocalGuide LocalGuideWidget">
-				
-				<s className='Read-More'>
-					<Link to={`/${country.fields.slug}/services`}>{t("global.See More", NS)}</Link>
-					<i className="material-icons">arrow_right</i>
-				</s>
-
-				<h3>{t("guide.Local Guide", NS)}</h3>
-
-				<div className="Container">
-				{guideItems.map(c => (
-					<div key={c.sys.id} className="LocalGuideItem">
-						<Link className="Overlay" to={`${c.fields.url}?language=${language}`}>
-							{_.has(c, 'fields.backgroundImage.fields.file.url') && <img src={c.fields.backgroundImage.fields.file.url} />}
-							<span style={{'position': 'absolute'}}>{c.fields.title}</span>
-							{/* {c.fields.iconClass && <i className={c.fields.iconClass}></i>} */}
-						</Link>
-					</div>
-				))}
-				</div>
-			</div>
-		);
-	}
+        <div className="Container">
+          {guideItems.map((c) => (
+            <div key={c.sys.id} className="LocalGuideItem">
+              <Link
+                className="Overlay"
+                to={`${c.fields.url}?language=${language}`}
+              >
+                {_.has(c, "fields.backgroundImage.fields.file.url") && (
+                  <img src={c.fields.backgroundImage.fields.file.url} />
+                )}
+                <span style={{ position: "absolute" }}>{c.fields.title}</span>
+                {/* {c.fields.iconClass && <i className={c.fields.iconClass}></i>} */}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="read-more-container">
+          <s className="Read-More">
+            <Link to={`/${country.fields.slug}/services`}>
+              {t("global.See More", NS)}
+            </Link>
+          </s>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default translate()(LocalGuideWidget);
