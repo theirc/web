9512677a6a49983cd33adc51becef46aa1cf407b
Guiso92 <emilioguisolfo@gmail.com>
2021-02-05 18:17:11 -0300

    fixed issues with translations

--- src/scenes/CountryHome/components/widgets/ArticleWidget.js
+++ src/scenes/CountryHome/components/widgets/ArticleWidget.js
@@ -1,124 +1,188 @@
 // libs
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { translate } from "react-i18next";
 import FacebookPlayer from "react-facebook-player";
 import YouTube from "react-youtube";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 // local
-import instance from '../../../../backend/settings';
+import instance from "../../../../backend/settings";
 import "./ArticleWidget.css";
 
-const NS = { ns: 'CountryHome' };
+const NS = { ns: "CountryHome" };
 
 const Remarkable = require("remarkable");
 const md = new Remarkable("full", {
-	html: true,
-	linkify: true,
-	typographer: true,
-	breaks: true,
+  html: true,
+  linkify: true,
+  typographer: true,
+  breaks: true,
 });
 
 class ArticleWidget extends Component {
-	static contextTypes = {
-		config: PropTypes.object,
-	};
+  static contextTypes = {
+    config: PropTypes.object,
+  };
 
-	renderVideo(article) {
-		const { url } = article.fields;
-		const APP_ID = instance.thirdParty.facebook.appId;
+  renderVideo(article) {
+    const { url } = article.fields;
+    const APP_ID = instance.thirdParty.facebook.appId;
 
-		if (/facebook.com/.test(url)) {
-			let videoId = url.replace(/.*facebook.com\/.*\/videos\/(.*)\/.*/, "$1");
-			return <FacebookPlayer className={"Facebook"} videoId={videoId} appId={APP_ID} />;
-		} else if (/youtube.com/) {
-			let videoId = url.replace(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/, "$7");
-			return <YouTube videoId={videoId} className={"YouTube"} />;
-		}
+    if (/facebook.com/.test(url)) {
+      let videoId = url.replace(/.*facebook.com\/.*\/videos\/(.*)\/.*/, "$1");
+      return (
+        <FacebookPlayer
+          className={"Facebook"}
+          videoId={videoId}
+          appId={APP_ID}
+        />
+      );
+    } else if (/youtube.com/) {
+      let videoId = url.replace(
+        /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+        "$7"
+      );
+      return <YouTube videoId={videoId} className={"YouTube"} />;
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	render() {
-		const { country, onNavigate, t, article, category, showHero, showFullArticle, language } = this.props;
-		console.log(instance.countries[country.fields.slug].thirdParty.facebook.messenger);
-		
-		// <a href={instance.countries[country.fields.slug].thirdParty.facebook.messenger} target="blank">PROBANDO</a>
+  render() {
+    const {
+      country,
+      onNavigate,
+      t,
+      article,
+      category,
+      showHero,
+      showFullArticle,
+      language,
+    } = this.props;
 
-		if (!article) {
-			// Anti pattern, but saves 1 or more ifs.
-			return null;
-		}
+    const whatsapp =
+      instance.countries[country.fields.slug].thirdParty.whatsapp;
+    const facebook =
+      instance.countries[country.fields.slug].thirdParty.facebook.messenger;
 
-		let categorySlug = "article";
-		if (category) {
-			categorySlug = category.fields.slug;
-		}
+    if (!article) {
+      // Anti pattern, but saves 1 or more ifs.
+      return null;
+    }
 
-		let content = showFullArticle ? article.fields.content : article.fields.lead;
-		let showFullArt = showFullArticle;
-		const { contentType } = article.sys;
+    let categorySlug = "article";
+    if (category) {
+      categorySlug = category.fields.slug;
+    }
 
-		if (contentType.sys.id === "video") {
-			content = article.fields.lead;
-			showFullArt = true;
-		}
-		let hero = article.fields.hero;
+    let content = showFullArticle
+      ? article.fields.content
+      : article.fields.lead;
+    let showFullArt = showFullArticle;
+    const { contentType } = article.sys;
 
-		md.renderer.rules.link_open = (tokens, idx /*, options, env */) => {
-			// var title = tokens[idx].title;
-			return `<a href="${tokens[idx].href}?language=${language}" ${tokens[idx].title}>`;
-		}
+    if (contentType.sys.id === "video") {
+      content = article.fields.lead;
+      showFullArt = true;
+    }
+    let hero = article.fields.hero;
 
-		const langLTR = ['ur', 'fa', 'ar'].indexOf(language) > -1;
+    md.renderer.rules.link_open = (tokens, idx /*, options, env */) => {
+      // var title = tokens[idx].title;
+      return `<a href="${tokens[idx].href}?language=${language}" ${tokens[idx].title}>`;
+    };
 
-		return (
-			<div className={`Article ArticleWidget Home ${langLTR ? 'asd' : 'efg'}`} key={article.sys.id}>
-				<div className="text-container">
-				{hero &&
-					hero.fields &&
-					hero.fields.file &&
-					showHero && (
-						<div className="hero">
-							<img className="heroImage" src={article.fields.hero.fields.file.url} alt="" />
-						</div>
-					)
-				}
+    const langLTR = ["ur", "fa", "ar"].indexOf(language) > -1;
 
-				<div className={`${!showFullArticle ? 'welcome-container' : ''} ${langLTR ? 'welcome-container-ltr' : ''}`}>
-				{showFullArt ? <h1>{article.fields.title}</h1> : <h1 onClick={() => onNavigate(`/${country.fields.slug}/${categorySlug}/${article.fields.slug}`)}>{article.fields.title}</h1>}
+    return (
+      <div
+        className={`Article ArticleWidget Home ${langLTR ? "asd" : "efg"}`}
+        key={article.sys.id}
+      >
+        {hero && hero.fields && hero.fields.file && showHero && (
+          <div className="hero">
+            <img
+              className="heroImage"
+              src={article.fields.hero.fields.file.url}
+              alt=""
+            />
+          </div>
+        )}
 
-				{contentType.sys.id === "video" && this.renderVideo(article)}
+        <div className={`${!showFullArticle ? "text-container" : ""}`}>
+          <div
+            className={`${!showFullArticle ? "welcome-container" : ""} ${
+              langLTR ? "welcome-container-ltr" : ""
+            }`}
+          >
+            {showFullArt ? (
+              <h1>{article.fields.title}</h1>
+            ) : (
+              <h1
+                onClick={() =>
+                  onNavigate(
+                    `/${country.fields.slug}/${categorySlug}/${article.fields.slug}`
+                  )
+                }
+              >
+                {article.fields.title}
+              </h1>
+            )}
 
-				
-				<p dangerouslySetInnerHTML={{ __html: md.render(content) }} />
-				
-				{!showFullArt &&
-					<s className="Read-More">
-						<Link to={`/${country.fields.slug}/${categorySlug}/${article.fields.slug}?language=${language}`}>{t("global.Read More", NS)}</Link>
-						{/* <i className="material-icons">arrow_right</i> */}
-					</s>
-				}
-				</div>
+            {contentType.sys.id === "video" && this.renderVideo(article)}
 
-				</div>
-				<div className="social-media-container">
-					<div className="whatsapp-container">
-						<i class="fa fa-whatsapp" />
-						<span></span>
-						<button></button>
-					</div>
-					<div className="facebook-container">
-						<i className="fa fa-facebook-messenger" />
-						<span></span>
-						<button></button>
-					</div>
-				</div>
+            <p dangerouslySetInnerHTML={{ __html: md.render(content) }} />
 
-			</div>
-		);
-	}
+            {!showFullArt && (
+              <s className="Read-More">
+                <Link
+                  to={`/${country.fields.slug}/${categorySlug}/${article.fields.slug}?language=${language}`}
+                >
+                  {t("global.Read More", NS)}
+                </Link>
+                {/* <i className="material-icons">arrow_right</i> */}
+              </s>
+            )}
+          </div>
+          {!showFullArt && (whatsapp || facebook) && (
+            <div className="social-media-container">
+              {whatsapp && (
+                <div className={`whatsapp-containerr ${!facebook ? 'only' : ''}`}>
+                  <div className="icon-container">
+                    <i className="fa fa-whatsapp" />
+                  </div>
+                  <h1>WhatsApp</h1>
+                  <span>
+                    Visit our page and use the chat to get your questions
+                    answered
+                  </span>
+                  <a target="blank" href={whatsapp}>
+                    WHATSAPP
+                  </a>
+                </div>
+              )}
+              {facebook && (
+                <div className={`facebook-container ${!whatsapp ? 'only' : ''}`}>
+                  <div className="icon-container">
+                    <i className="fa fa-facebook" />
+                  </div>
+                  <h1>Facebook Messenger</h1>
+                  <span>
+                    Visit our page and use the chat to get your questions
+                    answered
+                  </span>
+                  <a target="blank" href={facebook}>
+                    FACEBOOK
+                  </a>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default translate()(ArticleWidget);
