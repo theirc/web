10da73eaf9f021392800034eea901e7c002f01b4
spilafis <spilafis@users.noreply.github.com>
2021-11-15 10:54:54 -0300

    Enable loading of different faces
    Adding code to enable loading of different faces by query string param.   ex:  ?face=cuentanos

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,26 @@ import ki from './khabrona.info/instance';
 import ri from './refugee.info/instance';
 import iq from './simaetbhatha.com/instance';
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const face = urlParams.get('face')
+console.log("Loading face " + face);
+let azurewebsites = null;
+switch(face){
+	case 'khabrona':
+		azurewebsites = ki;
+		break;
+	case 'refugee':
+		azurewebsites = ri;
+		break;
+	case 'simaetbhatha':
+		azurewebsites = iq;
+		break;
+	default:
+		azurewebsites = cn;
+		break;
+}
+
 const instances = {
 	'cuentanos.org': cn,
 	'khabrona.info': ki,
